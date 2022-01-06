e87604bbd568c968e732cc53c57d6a907167918b
lifeoutro <5lf01rucom@inbox.ru>
2022-01-06 19:16:19 +0000

    yandex

--- file1.py
+++ file1.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 newslist = []
-page = requests.get('https://rambler.ru')
+useragent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'
+headers_ = {'User-Agent':useragent}
+page = requests.get('https://yandex.ru', headers=headers_)
 print(page.status_code)
+#print(page.text)
 if page.status_code == 200:
     soup = BeautifulSoup(page.text, "html.parser")
-    allnews = soup.findAll('a',class_ = '_2j90')
+    allnews = soup.findAll('span',class_ = 'news__item-content')
     for d in allnews:
         newslist.append(d.text)
     print(newslist)
